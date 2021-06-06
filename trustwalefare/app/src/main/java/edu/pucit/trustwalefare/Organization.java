3258b7d1b8471047978760de12c9dea232f9d28f
hawanha1 <bsef18m047@pucit.edu.pk>
2021-06-06 18:47:03 +0500

    recycler view apply for Organization activity

--- trustwalefare/app/src/main/java/edu/pucit/trustwalefare/Organization.java
+++ trustwalefare/app/src/main/java/edu/pucit/trustwalefare/Organization.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Organization extends AppCompatActivity {
+    String s1[];
+    int images[]={R.drawable.admin,R.drawable.donor,R.drawable.background,R.drawable.organization,R.drawable.member,R.drawable.admin,R.drawable.donor};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization);
+
+        s1=getResources().getStringArray(R.array.OrganizationNames);
     }
 }
