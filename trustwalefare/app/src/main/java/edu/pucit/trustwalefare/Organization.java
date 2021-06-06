e1aaa453808ade74db93b0e63b5d8b21b411e8c2
hawanha1 <bsef18m047@pucit.edu.pk>
2021-06-06 19:39:50 +0500

    organization recyclerView card rows java class and xml added

--- trustwalefare/app/src/main/java/edu/pucit/trustwalefare/Organization.java
+++ trustwalefare/app/src/main/java/edu/pucit/trustwalefare/Organization.java
@@ -1,18 +1,21 @@
 package edu.pucit.trustwalefare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Organization extends AppCompatActivity {
     String s1[];
     int images[]={R.drawable.admin,R.drawable.donor,R.drawable.background,R.drawable.organization,R.drawable.member,R.drawable.admin,R.drawable.donor};
-
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization);
-
+        recyclerView=findViewById(R.id.OrganizationRecyclerView);
         s1=getResources().getStringArray(R.array.OrganizationNames);
+
+        recyclerViewAdapter recyclerAdapter=new recyclerViewAdapter(this,s1,images);
     }
 }
