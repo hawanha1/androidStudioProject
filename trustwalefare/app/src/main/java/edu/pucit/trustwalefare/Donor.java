015629b93f9ff6195b98d48af9b54c7a2ef06749
hawanha1 <bsef18m047@pucit.edu.pk>
2021-06-08 12:23:06 +0500

    recycler view applied on donation screen

--- trustwalefare/app/src/main/java/edu/pucit/trustwalefare/Donor.java
+++ trustwalefare/app/src/main/java/edu/pucit/trustwalefare/Donor.java
@@ -1,14 +1,24 @@
 package edu.pucit.trustwalefare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Donor extends AppCompatActivity {
-
+    String s1[];
+    int images[]={R.drawable.admin,R.drawable.donor,R.drawable.background,R.drawable.organization,R.drawable.member,R.drawable.admin,R.drawable.donor};
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
+        recyclerView=findViewById(R.id.donationId);
+        s1=getResources().getStringArray(R.array.OrganizationNames);
+
+        recyclerViewAdapter recyclerAdapter=new recyclerViewAdapter(this,s1,images);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
